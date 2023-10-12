d09a4706de00104fc22518b916c4ace0448521e6
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-10-12 18:08:29 +0100

    Update code apiMissions

---
+++ src/API/apiMissions.jsx
@@ -0,0 +1,19 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const baseUrl = "https://api.spacexdata.com/v3/missions";
+
+const fetchMissions = createAsyncThunk(
+  "mission/fetchMissions",
+  async (thunkAPI) => {
+    try {
+      const res = await axios(baseUrl);
+      const data = await res.data;
+      return data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error.message);
+    }
+  }
+);
+
+export default fetchMissions;
