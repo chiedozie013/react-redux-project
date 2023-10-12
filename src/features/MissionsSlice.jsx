dbdc2b15716413bdd4d132ace7a166a8d30dcd4f
James Chiedozie James <110996482+chiedozie013@users.noreply.github.com>
2023-10-12 18:13:52 +0100

    Update features

---
+++ src/features/MissionsSlice.jsx
@@ -0,0 +1,36 @@
+import { createSlice } from "@reduxjs/toolkit";
+import fetchMissions from "../API/apiMissions";
+
+const initialState = {
+  missions: [],
+  loading: false,
+};
+
+const missionSlice = createSlice({
+  name: "mission",
+  initialState,
+  reducers: {
+    joinMission: (state, { payload }) => {
+      const updatedMissions = state.missions.map((mission) => {
+        if (mission.mission_id === payload) {
+          return { ...mission, reserved: !mission.reserved };
+        }
+        return mission;
+      });
+
+      return { ...state, missions: updatedMissions };
+    },
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(fetchMissions.pending, (state) => ({ ...state, loading: true }))
+      .addCase(fetchMissions.fulfilled, (state, { payload }) => ({
+        ...state,
+        missions: payload,
+      }));
+  },
+});
+
+export const { joinMission } = missionSlice.actions;
+
+export default missionSlice.reducer;
---
+++ src/features/rocketSlice.jsx
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+import fetchRockets from "../API/apiRockets";
+
+const initialState = {
+  rockets: [],
+  status: false,
+};
+
+const rocketsSlice = createSlice({
+  name: "rockets",
+  initialState,
+  reducers: {
+    reserveRocket: (state, { payload }) => {
+      const rockets = state.rockets.map((rocket) => {
+        if (rocket.id === payload)
+          return { ...rocket, reserved: !rocket.reserved };
+        return rocket;
+      });
+      return { ...state, rockets };
+    },
+  },
+  extraReducers(builder) {
+    builder
+      .addCase(fetchRockets.pending, (state) => ({ ...state, status: true }))
+      .addCase(fetchRockets.fulfilled, (state, action) => ({
+        ...state,
+        rockets: action.payload,
+      }));
+  },
+});
+
+export const { reserveRocket } = rocketsSlice.actions;
+
+export default rocketsSlice.reducer;
